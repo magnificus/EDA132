dfe6b1c53b9f6a3f217b546e13de65d4b1d14675
Tobias Elinder <tobiaselinder@gmail.com>
2016-02-10 12:32:39 +0100

    börjat

--- RobotLocalization/src/model/SmartyLocalizer.java
+++ RobotLocalization/src/model/SmartyLocalizer.java
@@ -2,46 +2,94 @@ package model;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import control.EstimatorInterface;
 
-public class SmartyLocalizer implements EstimatorInterface{
-	private ArrayList<Point> previousSensorLocation;
-	private Point currentLocation;
+public class SmartyLocalizer implements EstimatorInterface {
+	private ArrayList<Point> previous;
+	private Point currentTrueLocation;
+	private int rows, cols, head;
 	
-	
-	public SmartyLocalizer(){
-		previousSensorLocation = new ArrayList<Point>();
+	private static float firstNeighborChange = 0.05f;
+	private static float sndNeighborChance = 0.025f;
+	private static float trueChance = 0.1f;
+
+	public SmartyLocalizer(int rows, int cols, int head) {
+		this.cols = cols;
+		this.rows = rows;
+		this.head = head;
+		previous = new ArrayList<Point>();
+		Random rand = new Random();
+		currentTrueLocation = new Point(rand.nextInt(rows), rand.nextInt(cols));
 	}
 
 	@Override
 	public int getNumRows() {
-		// TODO Auto-generated method stub
-		return 0;
+		return rows;
 	}
 
 	@Override
 	public int getNumCols() {
-		// TODO Auto-generated method stub
-		return 0;
+		return cols;
 	}
 
 	@Override
 	public int getNumHead() {
-		// TODO Auto-generated method stub
-		return 0;
+		return head;
 	}
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
-		
+		previous.add(getNewPoint());
+
+	}
+
+	private Point getNewPoint() {
+		Random rand = new Random();
+		Float f = rand.nextFloat();
+		Float acc = 0f;
+		List<Point> firstNeighbors = getNeighBorsAtDist(1, currentTrueLocation);
+		List<Point> sndNeighbors = getNeighBorsAtDist(2, currentTrueLocation);
+		for (Point p : firstNeighbors){
+			acc+=firstNeighborChange;
+			if (f < acc){
+				return p;
+			}
+		}
+		for (Point p : sndNeighbors){
+			acc+=sndNeighborChance;
+			if (f < acc){
+				return p;
+			}
+		}
+		acc += trueChance;
+		if (f < acc){
+			return currentTrueLocation;
+		}
+		return new Point(-1,-1);
+	}
+
+	private List<Point> getNeighBorsAtDist(int dist, Point loc) {
+		List<Point> points = new ArrayList<Point>();
+		for (int i = loc.x - dist; i <= loc.x + dist; i++) {
+			for (int j = loc.y - dist; j <= loc.y + dist; j++) {
+				if (i - loc.x + j - loc.y == dist && inScope(loc.x, loc.y)) {
+					points.add(new Point(i, j));
+				}
+			}
+		}
+		return points;
+	}
+
+	private boolean inScope(int x, int y) {
+		return x >= 0 && x < rows && y >= 0 &&  y < cols;
 	}
 
 	@Override
 	public int[] getCurrentTruePosition() {
-		// TODO Auto-generated method stub
-		return null;
+		return new int[] { currentTrueLocation.x, currentTrueLocation.y };
 	}
 
 	@Override
