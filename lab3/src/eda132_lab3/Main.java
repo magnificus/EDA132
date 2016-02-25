83ecc2112cf494ed8ab178ce2cb782d3a7e36422
Tobias Elinder <tobiaselinder@gmail.com>
2016-02-25 12:03:22 +0100

    lab3

---
+++ lab3/src/eda132_lab3/Main.java
@@ -0,0 +1,15 @@
+package eda132_lab3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+public class Main {
+
+	
+	public static void main(){
+		 
+		 BufferedReader reader = new BufferedReader(
+		                              new FileReader("/some/where/data.arff"));
+		 reader.close();
+		 // setting class attribute
+	}
+}
